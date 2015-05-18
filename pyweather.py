caeab5ebb452d0fb8a3d128a81517c67f7abadc4
Asraf <asraf.alom02@gmail.com>
2015-05-18 19:50:10 +0000

    request response

--- pyweather.py
+++ pyweather.py
@@ -1,13 +1,20 @@
-#!/usr/bin/python
+    #!/usr/bin/python
+
+import urllib2
+
 
 def main():
     print "Hello World.."
+    url = 'https://developer.yahoo.com/'
     try:
-        # TODO: write code...
-    except Exception, e:
-        raise e
-    finally:
-        # TODO: write code...
+	    conn = urllib2.urlopen(url)
+	    #print "Http responce: %s" % conn.info()
+	    print "Http responce code: %d" % conn.getcode()
+	    data = conn.read()
+    except urllib2.HTTPError, e:
+	    print "HTTP error: %d" % e.code
+    except urllib2.URLError, e:
+	    print "Network error: %s" % e.reason.args[1]
 
 if __name__ == '__main__':
     main()
