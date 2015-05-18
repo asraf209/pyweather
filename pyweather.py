6ad5b5abe9fcc0a0f006c805481ad241546ce543
Asraf <asraf.alom02@gmail.com>
2015-05-18 19:26:11 +0000

    going forward with rewuest

---
+++ pyweather.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+def main():
+    print "Hello World.."
+    try:
+        # TODO: write code...
+    except Exception, e:
+        raise e
+    finally:
+        # TODO: write code...
+
+if __name__ == '__main__':
+    main()
