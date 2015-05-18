937dd508b5589837f94d79243ee0191fa873ac16
Asraf <asraf.alom02@gmail.com>
2015-05-18 19:54:15 +0000

    request response

--- pyweather.py
+++ pyweather.py
@@ -6,11 +6,13 @@ import urllib2
 def main():
     print "Hello World.."
     url = 'https://developer.yahoo.com/'
+    
     try:
 	    conn = urllib2.urlopen(url)
 	    #print "Http responce: %s" % conn.info()
 	    print "Http responce code: %d" % conn.getcode()
 	    data = conn.read()
+    
     except urllib2.HTTPError, e:
 	    print "HTTP error: %d" % e.code
     except urllib2.URLError, e:
