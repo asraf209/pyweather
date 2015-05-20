46a1db827b091179086aa4896d48cba74c008206
Md Asraful Alom <malom@luc.edu>
2015-05-19 22:43:02 -0500

    response is coming from worldweather

--- pyweather.py
+++ pyweather.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 
+# API Key: b0db249f8c79a7d7b581c4a7581d6bdf (openweather)
+# API Key: 2436ae911a0de75b8a932e6bd7edb    (weatherapi)
+
+
 import urllib2
 import sys
 
 def get_response(url):
     print "Hello World.."
     try:
-	    conn = urllib2.urlopen(url)
-	    #print "Http responce: %s" % conn.info()
-	    print "Http responce code: %d" % conn.getcode()
-	    data = conn.read()
-    
+        conn = urllib2.urlopen(url)
+        print "Http responce code: %d" % conn.getcode()
+        #print "Http responce: %s" % conn.info()        
+        data = conn.read()
+        return data	    
     except urllib2.HTTPError, e:
 	    print "HTTP error: %d" % e.code
 	    return None
@@ -23,11 +27,14 @@ def get_response(url):
 
 
 def process_data(data):
-    pass
+    print data
 
     
 def main():
-    response = get_response('https://developer.yahoo.com/')
+    url = 'http://api.worldweatheronline.com/free/v2/weather.ashx?key=2436ae911a0de75b8a932e6bd7edb&q=48.85,2.35&num_of_days=2&tp=3&format=json'
+    #response = get_response('https://developer.yahoo.com/')
+    #response = get_response('http://api.openweathermap.org/data/2.5/weather?q=Evanston,IL')
+    response = get_response(url)
     if response != None:
         process_data(response)
 
