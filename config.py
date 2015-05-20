d3551f6dbc06e9f5b8a303780b867ff7b0ca7ee8
Md Asraful Alom <malom@luc.edu>
2015-05-19 22:51:01 -0500

    making up url

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python
+url = 'http://api.worldweatheronline.com/free/v2/weather.ashx?key=2436ae911a0de75b8a932e6bd7edb&q=48.85,2.35&num_of_days=2&tp=3&format=json'
+base_url = 'http://api.worldweatheronline.com/free/v2/weather.ashx'
+api_key = '2436ae911a0de75b8a932e6bd7edb'
+url = base_url + '?' + 'key=' + api_key + '&q=48.85,2.35&num_of_days=2&tp=3&format=json'
